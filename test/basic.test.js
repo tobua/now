1dd2e10aaf99ed2ff3786c5c0026c4b741245f1b
Matthias Giger <i@matthiasgiger.com>
2020-08-19 14:53:57 +0200

    test(basic): add first tests and upgrade padua to include babel

---
+++ test/basic.test.js
@@ -0,0 +1,19 @@
+const { loadPackage } = require('./../utility/load-package.js')
+
+test('Returns correct url for various packages.', async () => {
+    let url = await loadPackage('padua')
+    expect(url).toEqual('tobua/padua')
+    url = await loadPackage('react')
+    expect(url).toEqual('facebook/react')
+})
+
+test('Fails for non-existent packages.', async () => {
+    const mockProcessExit = jest.spyOn(process, 'exit').mockImplementation(() => {
+        // Throw instead of exit, to stop execution.
+        throw new Error('Exit')
+    })
+
+    await expect(loadPackage('papua')).rejects.toEqual(new Error('Exit'))
+
+    expect(mockProcessExit).toHaveBeenCalledWith(0);
+})

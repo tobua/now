ec5d3137f795b787a0023d2f381d49d7d8a7e027
Matthias Giger <i@matthiasgiger.com>
2023-11-26 23:40:02 +0100

    fix(config): keep argument for cachePath in path
    release-npm

--- config.js
+++ config.js
@@ -3,9 +3,9 @@ import findCacheDir from 'find-cache-dir'
 import temporaryDirectory from 'temp-dir'
 
 const nodeModulesCacheDir = findCacheDir({ name: 'create-now', thunk: true })
-const npxCacheDirectory = (template) => join(temporaryDirectory, 'create-now', template)
+const npxCacheDirectory = () => join(temporaryDirectory, 'create-now')
 
-export function cachePath() {
-  const path = nodeModulesCacheDir ?? npxCacheDirectory
-  return typeof path === 'function' ? path() : path
+export function cachePath(name) {
+  const path = nodeModulesCacheDir ?? npxCacheDirectory()
+  return join(path, name)
 }
